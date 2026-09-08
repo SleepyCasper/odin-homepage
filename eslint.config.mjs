2b2f66d40bd179cc6a80a5a8e1048f9cf0b87b2f
Sofiia Riabova <sofia.ryabowa21@gmail.com>
2026-09-08 16:21:32 +0300

    Initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,27 @@
+import js from "@eslint/js";
+import globals from "globals";
+import { defineConfig } from "eslint/config";
+import eslintConfigPrettier from "eslint-config-prettier";
+
+export default defineConfig([
+  { 
+    files: ["**/*.{js,mjs,cjs}"], 
+    plugins: { 
+      js: js
+    }, 
+    extends: [
+      "js/recommended", 
+      eslintConfigPrettier
+    ], 
+    languageOptions: { 
+      globals: { 
+        ...globals.browser,
+        ...globals.jest
+      } 
+    },
+    rules: {
+      'no-unused-vars': 'warn',
+      'no-undef': 'warn',
+    }, 
+  },
+]);
