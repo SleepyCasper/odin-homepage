262673e91e695b76ae2286e86479d9cc5e9a3d04
Sofia <117224812+SleepyCasper@users.noreply.github.com>
2026-09-11 19:58:57 +0300

    Add layout, styles, animation and accessibility to home and projects sections.
    - Added SVG icons for social media and GitHub.
    - Included PNG images for various projects.
    - Created a new animation CSS file for various animations.
    - Updated style variables for improved color palette and font styles.
    - Enhanced main styles with responsive design and improved layout for projects section.
    - Updated HTML template with new content and structure for better accessibility and user experience.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,55 @@
 import "./styles/styles.css";
+
+const elements = {
+    projects: document.getElementById("container-projects"),
+
+}
+
+const Events = () => {
+    elements.projects.addEventListener("mouseover", (e) => {
+        showDetails(e)
+    })
+
+    elements.projects.addEventListener("focusin", (e) => {
+        showDetails(e)
+    })
+
+    elements.projects.addEventListener("mouseout", (e) => {
+        hideDetails(e)
+    })
+
+    elements.projects.addEventListener("focusout", (e) => {
+        hideDetails(e)
+    })
+
+    function showDetails(e) {
+        const window = e.target.closest(".project-preview")
+
+        if (window) {
+            const details = window.querySelector(".details")
+            details.classList.remove("hide");
+            details.classList.add("show");
+        }
+    }
+
+    function hideDetails(e) {
+        const window = e.target.closest(".project-preview")
+
+        if (window) {
+            const details = window.querySelector(".details")
+            if (!details.classList.contains("show")) return;
+
+            details.classList.remove("show");
+            details.classList.add("hide");
+
+            details.addEventListener(
+                "animationend",
+                () => details.classList.remove("hide"),
+                { once: true }
+            );
+        }
+    }
+
+}
+
+Events()
